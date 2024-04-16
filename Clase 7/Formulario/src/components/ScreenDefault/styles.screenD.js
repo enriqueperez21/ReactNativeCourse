782ab5b5f0704197f87068c9124bc81def64421e
Luis Enrique Pérez Señalin <luis.perez05@epn.edu.ec>
2024-04-16 10:06:08 -0500

    Login Formulario

--- Clase 7/Formulario/src/components/ScreenDefault/styles.screenD.js
+++ Clase 7/Formulario/src/components/ScreenDefault/styles.screenD.js
@@ -3,7 +3,5 @@ import { StyleSheet } from "react-native";
 export const StyleDefault = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
     }
 })
